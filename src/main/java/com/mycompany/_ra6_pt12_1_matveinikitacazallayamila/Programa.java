76ed3012cb28be528818d03633f7503ca7b6fe55
nikita <nikitamatpech@gmail.com>
2025-05-06 10:44:02 +0200

    Versió incial del programa

---
+++ src/main/java/com/mycompany/_ra6_pt12_1_matveinikitacazallayamila/Programa.java
@@ -0,0 +1,12 @@
+package com.mycompany._ra6_pt12_1_matveinikitacazallayamila;
+
+/**
+ *
+ * @author Nikita i Yamila
+ */
+public class Programa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
